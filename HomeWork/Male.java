56d6209b4534da18274bab99b4286f1462022ff8
Nikita <nikita.lapin.201248@gmail.com>
2018-04-25 00:22:30 +0300

    Add files via upload

---
+++ HomeWork/Male.java
@@ -0,0 +1,7 @@
+package HomeWork;
+
+public class Male extends Human {
+    public Male(boolean gender, String name, String surname, float height, float weight) {
+        super(true, name, surname, height, weight);
+    }
+}
